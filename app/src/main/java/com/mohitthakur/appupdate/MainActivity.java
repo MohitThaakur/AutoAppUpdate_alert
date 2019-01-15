45b7e069892a5beacd18a37a3c6268f52debd54f
Mohit Singh <ms1211112.ms@gmail.com>
2019-01-15 19:24:50 +0530

    Initial commit

---
+++ app/src/main/java/com/mohitthakur/appupdate/MainActivity.java
@@ -0,0 +1,107 @@
+package com.mohitthakur.appupdate;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseRemoteConfig mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+    private HashMap<String, Object> firebaseDefaultMap;
+    public static final String VERSION_CODE_KEY = "gsgdh";
+    private static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        firebaseDefaultMap = new HashMap<>();
+        firebaseDefaultMap.put(VERSION_CODE_KEY, getCurrentVersionCode());
+        mFirebaseRemoteConfig.setDefaults(firebaseDefaultMap);
+
+        //Setting that default Map to Firebase Remote Config
+
+        //Setting Developer Mode enabled to fast retrieve the values
+        mFirebaseRemoteConfig.setConfigSettings(
+                new FirebaseRemoteConfigSettings.Builder().setDeveloperModeEnabled(BuildConfig.DEBUG)
+                        .build());
+
+        //Fetching the values here
+        mFirebaseRemoteConfig.fetch(60*30).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    mFirebaseRemoteConfig.activateFetched();
+                    Log.d(TAG, "Fetched value: " + mFirebaseRemoteConfig.getString(VERSION_CODE_KEY));
+                    //calling function to check if new version is available or not
+                    checkForUpdate();
+                } else {
+                    Toast.makeText(MainActivity.this, "Something went wrong please try again",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Log.d(TAG, "Default value: " + mFirebaseRemoteConfig.getString(VERSION_CODE_KEY));
+
+
+
+
+    }
+
+    private void checkForUpdate() {
+        int latestAppVersion = (int) mFirebaseRemoteConfig.getDouble(VERSION_CODE_KEY);
+        if (latestAppVersion > getCurrentVersionCode()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            View inflate = getLayoutInflater().inflate(R.layout.activity_updater, null);
+            builder.setView(inflate);
+            TextView textView = (TextView) inflate.findViewById(R.id.yes);
+            AlertDialog create = builder.create();
+            create.setCancelable(false);
+            create.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+            create.show();
+
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    MainActivity.this.startActivity(new Intent("android.intent.action.VIEW", Uri.parse("")));
+                    MainActivity.this.finish();
+
+                }
+            });
+
+        }
+    }
+
+    private int getCurrentVersionCode() {
+        try {
+            return getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+}
---
+++ app/src/main/java/com/mohitthakur/appupdate/Updater.java
@@ -0,0 +1,13 @@
+package com.mohitthakur.appupdate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Updater extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updater);
+    }
+}
