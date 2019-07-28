c4ffeb2cc14c2582192a3ffccf4ffbae9b6a50c0
Mohit Thakur <36338537+mohit0056@users.noreply.github.com>
2019-07-29 00:54:12 +0530

    Delete hiddenQuotesjson.php

--- hiddenQuotesjson.php
+++
@@ -1,9 +0,0 @@
-{"status":"true","message":"Data fetched successfully!",
-"data":[{"id":"1","name":"Stay Tuned for my live session ","imgURL":"https://scontent.fagr1-1.fna.fbcdn.net/v/t1.0-9/27067410_1551228148259238_8038187302688575723_n.jpg?_nc_cat=104&_nc_oc=AQmGF3zxGLxgH3PcvxLRf-Rz2Ww4Ze3igNLwn0qS9qBnq0bQmTIyb34aouKZDBVqMzxZRmZU2ZBgn3m60eA_VFQt&_nc_ht=scontent.fagr1-1.fna&oh=13502a4884d31ce30e4cf5c0b4ade7d2&oe=5DAAA653"},
-{"id":"2","name":"Rafael Nadal","imgURL":"https://scontent.fagr1-1.fna.fbcdn.net/v/t1.0-9/50496123_1987440274638021_5758150821388746752_n.jpg?_nc_cat=109&_nc_oc=AQm9ESGYFHlfr7MdMNc5HB2Q3j0laugDoEH1gD4AUlc9iGq7C_VyoBJYSKk0Y63_3JAa8_7_01-YSBrw00W7tKmT&_nc_ht=scontent.fagr1-1.fna&oh=c6d69c384805fd73f8795ffb91a343be&oe=5DB17871"},
-{"id":"3","name":"Novak Djokovic","imgURL":"https://scontent.fagr1-1.fna.fbcdn.net/v/t1.0-9/62409932_2195657663816280_2586636284420161536_n.jpg?_nc_cat=105&_nc_oc=AQm9S0g2TsNT8sXJ7i_gM7d_zCBAaRtr_46SVLpuQAyWWFsOxnfW8l8yclp0z_z3OQOzemKgZ_mSnzyRaISXHhvq&_nc_ht=scontent.fagr1-1.fna&oh=a972788e7a16d91152d8d5d035e0911d&oe=5DEC6DF6"},
-{"id":"4","name":"Andy Murray","imgURL":"https:\/\/demonuts.com\/Demonuts\/SampleImages\/murray.jpg"},
-{"id":"5","name":"Maria Sharapova","imgURL":"https:\/\/demonuts.com\/Demonuts\/SampleImages\/shara.jpg"},
-{"id":"6","name":"Caroline Wozniacki","imgURL":"https:\/\/demonuts.com\/Demonuts\/SampleImages\/woz.jpg"},
-{"id":"7","name":"Eugenie Bouchard","imgURL":"https:\/\/demonuts.com\/Demonuts\/SampleImages\/bou.png"},
-{"id":"8","name":"Ana Ivanovic","imgURL":"https:\/\/demonuts.com\/Demonuts\/SampleImages\/iva.jpg"}]}
