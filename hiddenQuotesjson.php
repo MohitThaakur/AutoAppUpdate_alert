50a2e3d40171c6a09798eeded4e20d445554a05a
Mohit Thakur <36338537+mohit0056@users.noreply.github.com>
2019-07-29 00:54:36 +0530

    Add files via upload

---
+++ hiddenQuotesjson.php
@@ -0,0 +1,9 @@
+{"status":"true","message":"Data fetched successfully!",
+"data":[{"id":"1","name":"Stay Tuned for my live session ","imgURL":"https:\/\/demonuts.com\/Demonuts\/SampleImages\/murray.jpg"},
+{"id":"2","name":"Rafael Nadal","imgURL":"https:\/\/demonuts.com\/Demonuts\/SampleImages\/murray.jpg"},
+{"id":"3","name":"Novak Djokovic","imgURL":"https:\/\/demonuts.com\/Demonuts\/SampleImages\/murray.jpg"},
+{"id":"4","name":"Andy Murray","imgURL":"https:\/\/demonuts.com\/Demonuts\/SampleImages\/murray.jpg"},
+{"id":"5","name":"Maria Sharapova","imgURL":"https:\/\/demonuts.com\/Demonuts\/SampleImages\/shara.jpg"},
+{"id":"6","name":"Caroline Wozniacki","imgURL":"https:\/\/demonuts.com\/Demonuts\/SampleImages\/woz.jpg"},
+{"id":"7","name":"Eugenie Bouchard","imgURL":"https:\/\/demonuts.com\/Demonuts\/SampleImages\/bou.png"},
+{"id":"8","name":"Ana Ivanovic","imgURL":"https:\/\/demonuts.com\/Demonuts\/SampleImages\/iva.jpg"}]}
